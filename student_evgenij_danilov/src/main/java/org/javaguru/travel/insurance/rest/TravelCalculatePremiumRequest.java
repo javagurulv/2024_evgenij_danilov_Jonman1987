ca4896981aa62d81c4594b99fe375d2043b03883
Jonman <Jonman@yandex.ru>
2024-06-17 17:03:18 +0700

    STEP 010 - FIX 01

--- student_evgenij_danilov/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ student_evgenij_danilov/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -3,7 +3,6 @@ package org.javaguru.travel.insurance.rest;
 import java.util.Date;
 
 public class TravelCalculatePremiumRequest {
-
     private String personFirstName;
     private String personLastName;
     private Date agreementDateFrom;
