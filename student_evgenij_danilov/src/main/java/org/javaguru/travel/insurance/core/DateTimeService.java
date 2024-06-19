1b343bd66ad496abbad8e1f51319ef2d45881f5e
Jonman <Jonman@yandex.ru>
2024-06-19 21:38:01 +0700

    STEP 019 - FIX 04

--- student_evgenij_danilov/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
+++ student_evgenij_danilov/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
@@ -8,28 +8,8 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class DateTimeService {
-    long daysCount;
-
-    public DateTimeService(){
-
-    }
-
-    public DateTimeService(Date agreementDateFrom, Date agreementDateTo){
-        daysCount = calculateDaysCount(agreementDateFrom, agreementDateTo);
-    }
-
-    public long getDaysCount(){
-        return daysCount;
-    }
-
-    public void setDaysCount(long daysCount){
-        this.daysCount = daysCount;
-    }
-
     public long calculateDaysCount(Date agreementDateFrom, Date agreementDateTo){
-        daysCount = TimeUnit.DAYS.convert(agreementDateTo.getTime()
+        return TimeUnit.DAYS.convert(agreementDateTo.getTime()
                 - agreementDateFrom.getTime(), TimeUnit.MILLISECONDS);
-
-        return daysCount; // Сделал возвращаемое значение, чтобы можно было увидеть результат калькуляции
     }
 }
--- student_evgenij_danilov/src/test/java/org/javaguru/travel/insurance/core/DateTimeServiceTest.java
+++ student_evgenij_danilov/src/test/java/org/javaguru/travel/insurance/core/DateTimeServiceTest.java
@@ -7,25 +7,10 @@ import java.util.Date;
 
 public class DateTimeServiceTest {
     @Test
-    public void getAgreementPriceTest(){
-        DateTimeService dateTimeService = new DateTimeService(new Date(2023, 10, 27), new Date(2023, 10, 28));
-
-        Assertions.assertEquals(dateTimeService.getDaysCount(), 1);
-    }
-
-    @Test
-    public void setAgreementPriceTest(){
-        DateTimeService dateTimeService = new DateTimeService();
-        dateTimeService.setDaysCount(1L);
-
-        Assertions.assertEquals(dateTimeService.getDaysCount(), 1);
-    }
-
-    @Test
     public void calculateAgreementPriceTest(){
         DateTimeService dateTimeService = new DateTimeService();
-        dateTimeService.calculateDaysCount(new Date(2023, 10, 27), new Date(2023, 10, 28));
+        long days = dateTimeService.calculateDaysCount(new Date(2023, 10, 27), new Date(2023, 10, 28));
 
-        Assertions.assertEquals(dateTimeService.getDaysCount(), 1);
+        Assertions.assertEquals(days, 1);
     }
 }
