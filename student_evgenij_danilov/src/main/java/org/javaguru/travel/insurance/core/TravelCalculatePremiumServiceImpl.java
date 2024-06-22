cf8fa368de8c7d7c021ad1c9c1d6acb63704c75d
Jonman <Jonman@yandex.ru>
2024-06-22 22:27:07 +0700

    STEP 033 - FIX STEP 28

--- student_evgenij_danilov/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_evgenij_danilov/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -4,18 +4,28 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
+import org.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+
     private final DateTimeService dateTimeService;
+    private final TravelCalculatePremiumRequestValidator requestValidator;
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = requestValidator.validate(request);
+        if (!errors.isEmpty()) {
+            return new TravelCalculatePremiumResponse(errors);
+        }
+
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setAgreementDateTo(request.getAgreementDateTo());
         response.setPersonFirstName(request.getPersonFirstName());
